b6c588eade66260ffca112fe01bdbdf40fa4791c
Bryan Correia <bryanmotta7@gmail.com>
2020-02-28 17:40:55 -0300

    error

--- lib/components/my_card.dart
+++ lib/components/my_card.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MyCard extends StatelessWidget {
-  String _title;
-  String _description;
+  int id;
+  String title;
+  String description;
 
-  MyCard(this._title, this._description);
+  MyCard(this.title, this.description);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.nfc),
-        title: Text(this._title),
-        subtitle: Text(this._description),
+        title: Text(this.title),
+        subtitle: Text(this.description),
       ),
     );
   }
--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,14 +1,42 @@
+import 'package:nucard/components/my_card.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-void createDatabase(){
-  getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    openDatabase(path, onCreate: (db, version){
-      db.execute('CREATE TABLE contacts('
+Future<Database> createDatabase() {
+  return getDatabasesPath().then((dbPath) {
+    final String path = join(dbPath, 'nucard.db');
+    return openDatabase(path, onCreate: (db, version) {
+      db.execute('CREATE TABLE myCard('
           'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'account_number INTEGER)');
+          'title TEXT, '
+          'description INTEGER)');
     }, version: 1);
+  });
+}
+
+void save(MyCard myCard) {
+  createDatabase().then((db) {
+    final Map<String, dynamic> myCardMap = Map();
+    myCardMap['id'] = myCard.id;
+    myCardMap['title'] = myCard.title;
+    myCardMap['description'] = myCard.description;
+
+    db.insert('myCard', myCardMap);
+  });
+}
+
+Future<MyCard> findAll() {
+  return createDatabase().then((db) {
+    return db.query('myCards').then((maps) {
+      final List<MyCard> myCards = List();
+      for (Map<String, dynamic> map in maps) {
+        final MyCard myCard = MyCard(
+          map['title'],
+          map['description'],
+        );
+        myCards.add(myCard);
+      }
+      return myCards;
+    });
   });
 }
