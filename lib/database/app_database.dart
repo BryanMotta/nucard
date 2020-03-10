17e6cd1b7991c0a3aee7ac5be1fcfccd1b7a30ee
Bryan Motta <bryanmotta7@gmail.com>
2020-03-09 21:38:06 -0300

    até aqui funciona com o find all

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,49 +2,42 @@ import 'package:nucard/components/my_card.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'nucard.db');
+Future<Database> getDatabase() async {
+    final String path = join(await getDatabasesPath(), 'nucard.db');
     return openDatabase(path, onCreate: (db, version) {
       db.execute('CREATE TABLE mycard('
           'id INTEGER PRIMARY KEY, '
           'title TEXT, '
           'description INTEGER)');
-    }, version: 1,
-        onDowngrade: onDatabaseDowngradeDelete);
-  });
+    }, version: 1);
 }
 
-Future<int> save(MyCard myCard) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> myCardMap = Map();
-    myCardMap['id'] = myCard.id;
-    myCardMap['title'] = myCard.title;
-    myCardMap['description'] = myCard.description;
+Future<int> save(MyCard myCard) async {
+  final Database db = await getDatabase();
 
-    return db.insert('mycard', myCardMap);
-  });
+  final Map<String, dynamic> myCardMap = Map();
+  myCardMap['id'] = myCard.id;
+  myCardMap['title'] = myCard.title;
+  myCardMap['description'] = myCard.description;
+
+  return db.insert('mycard', myCardMap);
 }
 
-Future<List<MyCard>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('mycard').then((maps) {
-      final List<MyCard> myCards = List();
-      for (Map<String, dynamic> map in maps) {
-        final MyCard myCard = MyCard(
-          map['title'],
-          map['description'],
-        );
-        myCards.add(myCard);
-      }
-      return myCards;
-    });
-  });
+Future<List<MyCard>> findAll() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('mycard');
+  final List<MyCard> listMyCards = List();
+  for (Map<String, dynamic> row in result) {
+    final MyCard myCard = MyCard(
+      row['title'],
+      row['description'],
+    );
+    listMyCards.add(myCard);
+  }
+  return listMyCards;
 }
 
-Future<void> deleteDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'nucard.db');
-    return databaseFactory.deleteDatabase(path);
-  });
+Future<void> deleteDatabase() async{
+  final String path = join(await getDatabasesPath(), 'nucard.db');
+  return databaseFactory.deleteDatabase(path);
 }
