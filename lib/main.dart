3c28229cb7a4ca9b4c29d634b64bab1d91427483
Bryan Correia <bryanmotta7@gmail.com>
2020-03-13 17:37:36 -0300

    CHANNEL

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:nucard/components/my_card.dart';
-import 'package:nucard/database/app_database.dart';
+import 'package:flutter/services.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
 import 'package:nucard/screens/my_cards.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(NuCard());
+  runApp(BatteryHome());
 }
 
 class NuCard extends StatelessWidget {
@@ -18,4 +24,52 @@ class NuCard extends StatelessWidget {
   }
 }
 
+class BatteryHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyBatteryHomeState();
+  }
+}
 
+class MyBatteryHomeState extends State<BatteryHome> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text('Qual a porcentagem de bateria?')),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                RaisedButton(
+                  child: Text('Get Battery Level'),
+                  onPressed: _getBatteryLevel,
+                ),
+                Text(_batteryLevel),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
